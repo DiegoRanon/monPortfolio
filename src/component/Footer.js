19a2c95ff7c44386e58580f15ebaccd417c3603d
Konan <mastercelis8891@gmail.com>
2023-10-24 21:25:01 -0400

    Initialisation page

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg"
+import logo from "../assets/img/git-commit-svgrepo-com.svg"
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
+import github from '../assets/img/github-mark.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 export const Footer = () => {
     return(
@@ -9,13 +10,11 @@ export const Footer = () => {
             <Container>
                 <Row className="align-item-center">
                     <Col sm={6}>
-                        <img src={logo} alt="logo" />
                     </Col>
                     <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href=""><img src={navIcon1} /></a>
-                            <a href=""><img src={navIcon2} /></a>
-                            <a href=""><img src={navIcon3} /></a>
+                            <a href="https://www.linkedin.com/in/diego-ranon-986b0120a/" target="_blank"><img src={navIcon1} /></a>
+                            <a href="https://github.com/DiegoRanon" target="_blank"><img src={github} /></a>
                         </div>
                     </Col>
                     <p>CopyRight 2022. All Right Reserved By Diego Ranon</p>
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,8 +1,8 @@
 import {useState, useEffect} from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/git-commit-svgrepo-com.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import github from '../assets/img/github-mark.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
 
@@ -45,9 +45,8 @@ export const NavBar = () => {
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt=""></img></a>
-                            <a href="#"><img src={navIcon2} alt=""></img></a>
-                            <a href="#"><img src={navIcon3} alt=""></img></a>
+                            <a href="https://www.linkedin.com/in/diego-ranon-986b0120a/" target="_blank"><img src={navIcon1} alt=""></img></a>
+                            <a href="https://github.com/DiegoRanon" target="_blank"><img src={github} alt=""></img></a>
                         </div>
                         <button className="vvd" onClick={()=>console.log('connect')}><span>Let's Connect</span></button>
                     </span>
